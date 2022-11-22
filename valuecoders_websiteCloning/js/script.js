90519ff7123767e6e68ae1a6b1eb9be121938a45
apton-jeevan <jeevan.kurian@aptonworks.com>
2022-11-22 17:24:44 +0530

    megamenu dropdown menu herosection and ftr completed

--- valuecoders_websiteCloning/js/script.js
+++ valuecoders_websiteCloning/js/script.js
@@ -1,7 +1,28 @@
-let megacontent=document.getElementsByClassName("changemega");
-let arr=Array.from(megacontent);
-arr.shift();
-arr.forEach((elem)=>{
-   //use eventlistener
+//for changing all rightmega to none except the first one
+for(i=1;i<5;i++){
+   document.getElementById("r"+ i).style="display:none"
 }
-)
+document.getElementById("r0").style="display:flex"
+
+let leftmega = document.getElementsByClassName("changemega");
+let arrleft = Array.from(leftmega);
+//for changing righmega when hovered on its respective leftmega
+arrleft.forEach((element,index)=>{
+   element.addEventListener("mouseover",()=>{
+
+      let right=document.getElementsByClassName("rightmega");
+      let arrright=Array.from(right);
+      arrright.forEach((e)=>{
+         if(e.id!="r" + index)
+         {
+            e.style="display:none";
+         }
+         else{
+            e.style="display:flex";
+         }
+      })
+    
+   })
+   
+})
+
