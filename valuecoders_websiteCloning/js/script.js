e18da5deba778993ca96bd1da99673b650dcd313
apton-jeevan <jeevan.kurian@aptonworks.com>
2022-11-29 15:03:00 +0530

    NOV29 added footer to both index.html and contact.html and made whole site responsive till now

--- valuecoders_websiteCloning/js/script.js
+++ valuecoders_websiteCloning/js/script.js
@@ -44,3 +44,76 @@ crossicon.addEventListener("click",()=>{
 
 })
 
+//for validating form 
+
+function validateform()
+{
+    returnval=true;
+    // validating name
+    let naam=document.getElementById("uname").value
+    if (naam.length<=2)
+    { 
+        err=document.getElementById("nameerror")
+        err.innerHTML="name should be atleast 3 characters"
+        returnval=false;
+    }
+    else{
+        err=document.getElementById("nameerror") //if we do not give else conditons then error once displayed will stay there even if required condtions for that particular input is met
+        err.innerHTML=""
+
+    }
+ 
+    // validating phone num
+    let pno=document.getElementById("phonenum").value
+
+    if(/^[0-9.,]+$/.test(pno)==false)
+    {
+        err=document.getElementById("numerror")
+        err.innerHTML="phone no should contain only digits"
+        returnval=false;
+    }
+     
+    else if (pno.length!=10)
+    { 
+        err=document.getElementById("numerror")
+        err.innerHTML="phone no should contain 10 characters"
+        returnval=false;
+    }
+    else{
+        err=document.getElementById("numerror")
+        err.innerHTML=""
+
+    }
+
+    if(returnval)
+    {
+     alert("Form has been submitted sucessfully");
+     
+   }
+   event.preventDefault();
+    if(returnval){
+      //code to send email
+        var params = {
+          name: document.getElementById("uname").value,
+          email: document.getElementById("email").value,
+          Country: document.getElementById("country").value,                                                                            
+          PhoneNum: document.getElementById("phonenum").value                                                                             
+        };
+      
+        const serviceID = "service_j5kwbqk";
+        const templateID = "template_c6hmtam";
+          emailjs.send(serviceID, templateID, params).then(()=>{
+              document.getElementById("uname").value = "";
+              document.getElementById("email").value = "";
+              document.getElementById("country").value = "";
+              document.getElementById("phonenum").value = "";
+              alert("Your message sent successfully!!")
+              
+          })
+          return returnval;
+    }
+     
+    }
+
+
+
